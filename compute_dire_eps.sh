1fcd0a41bf1356df8524b54a0b7d2cc932cb3a7f
Ctushar721 <tushar.choudhary010@gmail.com>
2024-10-13 17:49:36 +0530

    default params modified

--- compute_dire_eps.sh
+++ compute_dire_eps.sh
@@ -1,12 +1,12 @@
 ## set MODEL_PATH, num_samples, has_subfolder, images_dir, recons_dir, dire_dir
-DATA_ROOT=("/content/drive/MyDrive/DistilDire/dalle3_males")
-SAVE_ROOT=("/content/drive/MyDrive/DistilDire/dalle3_males")
+DATA_ROOT=("/content/drive/MyDrive/DistilDire/dataset/dalle3_males", "/content/drive/MyDrive/DistilDire/dataset/dalle3_females", "/content/drive/MyDrive/DistilDire/dataset/midjourney_males", "/content/drive/MyDrive/DistilDire/dataset/midjourney_females", "/content/drive/MyDrive/DistilDire/dataset/sd1_males", "/content/drive/MyDrive/DistilDire/dataset/sd1_females")
+SAVE_ROOT=("/content/drive/MyDrive/DistilDire/dataset/dalle3_males", "/content/drive/MyDrive/DistilDire/dataset/dalle3_females", "/content/drive/MyDrive/DistilDire/dataset/midjourney_males", "/content/drive/MyDrive/DistilDire/dataset/midjourney_females", "/content/drive/MyDrive/DistilDire/dataset/sd1_males", "/content/drive/MyDrive/DistilDire/dataset/sd1_females")
 
 MODEL_PATH="/content/drive/MyDrive/DistilDire/AdmModel/256x256_diffusion_uncond.pt" # imagenet pretrained adm (unconditional, 512x512)
 SAMPLE_FLAGS="--batch_size 32" # ddim20 is forced
 PREPROCESS_FLAGS="--compute_dire True --compute_eps True"
 
-for i in 0 
+for i in 0 1 2 3 4 5
 do
     SAVE_FLAGS="--data_root ${DATA_ROOT[$i]} --save_root ${SAVE_ROOT[$i]}"
     echo "Running on ${DATA_ROOT[$i]} with save root ${SAVE_ROOT[$i]}"
