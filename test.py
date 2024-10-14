466c91f891037f18be841eb086e926561be304d2
Ctushar721 <tushar.choudhary010@gmail.com>
2024-10-14 09:20:05 +0530

    logs added

--- test.py
+++ test.py
@@ -13,9 +13,12 @@ def main(run, cfg):
     dataloader = DataLoader(dataset, 
                             batch_size=1, 
                             shuffle=True, num_workers=2)
+    print("Dataloader done")
     trainer = Trainer(cfg, dataloader, dataloader, run, 0, False, 1)
     assert len(cfg.pretrained_weights) != 0, "Give proper checkpoint path"
+    print("trainer loaded")
     trainer.load_networks(cfg.pretrained_weights)
+    print("network loaded")
     trainer.validate(False, save=True, save_name=f"{cfg.root_dir}/{cfg.datasets_test}_{cfg.pretrained_weights.split('/')[-1]}_results.txt")
 
 if __name__ == "__main__":
